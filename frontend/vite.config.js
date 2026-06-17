eef7c7f636862e49f53fe9f21da582ffb18018b6
yumachin2003 <yumachin2003@gmail.com>
2026-06-17 10:41:12 +0900

    tunnelコマンドの追加

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'https://localhost:5001',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: true,
         secure: false,
       }
