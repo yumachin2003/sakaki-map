ff1ffc403baa4ef9bb060eb36847109234c17281
yumachin2003 <yumachin2003@gmail.com>
2026-07-08 22:55:43 +0900

    v0.1: Dockerイメージ構築用のファイルを作成・GitHub Actionで自動デプロイシステムを作成

--- frontend/src/lv2.js
+++ frontend/src/lv2.js
@@ -5,10 +5,8 @@
  *  ④ 最終的には全て true の状態にする。
  */
 
-import { IconTipJarEuro } from "@tabler/icons-react";
-
 export const useSatellite = false;      // マップのレイヤー切り替えボタンを表示する
 export const useCurrentButton = false;  // 現在地ボタンを表示する
 export const useSearchBar = false;      // 検索ボックスを表示する
 export const useDetailCard = false;     // 思い出の詳細カードを有効にする
-export const useUploadPage = false;     // データ送信ページを有効にする
+export const useUploadPage = false     // データ送信ページを有効にする
